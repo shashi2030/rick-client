709f1c6db53b3a7c772f88cb43947bdefacf00eb
Shashi Singh <shasingh36@publicisgroupe.net>
2020-06-02 17:16:01 +0530

    remove build gtom git ignore

---
+++ build/precache-manifest.55cf8c1865df67e081fa5aa9d5f551c3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c777f10e48e07609f85c7719cfc62467",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c0794e49885cbbeb8c4b",
+    "url": "/static/css/main.f80badc8.chunk.css"
+  },
+  {
+    "revision": "c51878ebcd86c41cd2a9",
+    "url": "/static/js/2.00f2cfa4.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.00f2cfa4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0794e49885cbbeb8c4b",
+    "url": "/static/js/main.8588fb7e.chunk.js"
+  },
+  {
+    "revision": "787b0a75af12f84c2283",
+    "url": "/static/js/runtime-main.15dcafb5.js"
+  },
+  {
+    "revision": "e75855a18e3532ccada0d25685656a95",
+    "url": "/static/media/logout.e75855a1.svg"
+  }
+]);
