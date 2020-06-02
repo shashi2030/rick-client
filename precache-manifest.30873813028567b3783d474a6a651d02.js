4b5a923defeb8ba8f525264dc1299a26917ec947
Shashi Singh <shasingh36@publicisgroupe.net>
2020-06-02 18:05:17 +0530

    Updates

---
+++ precache-manifest.30873813028567b3783d474a6a651d02.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1735f8f94cec24202ff7",
+    "url": "/rick-client/./static/css/main.f80badc8.chunk.css"
+  },
+  {
+    "revision": "c51878ebcd86c41cd2a9",
+    "url": "/rick-client/./static/js/2.00f2cfa4.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/rick-client/./static/js/2.00f2cfa4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1735f8f94cec24202ff7",
+    "url": "/rick-client/./static/js/main.9bcf2d40.chunk.js"
+  },
+  {
+    "revision": "aee1911fea4f32b637ae",
+    "url": "/rick-client/./static/js/runtime-main.f296e6ee.js"
+  },
+  {
+    "revision": "e75855a18e3532ccada0d25685656a95",
+    "url": "/rick-client/./static/media/logout.e75855a1.svg"
+  },
+  {
+    "revision": "e0c1c8c9855d08c40129af083641bb3f",
+    "url": "/rick-client/index.html"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.55cf8c1865df67e081fa5aa9d5f551c3.js"
+  "/rick-client/precache-manifest.30873813028567b3783d474a6a651d02.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/rick-client/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
